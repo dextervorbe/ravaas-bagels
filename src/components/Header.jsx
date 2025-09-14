255e921930f024a36f51d01bcbce51c1768de5cb
Dexter Vorbe <dextervorbe@ymail.com>
2025-09-14 19:15:26 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
             <Link
               to="/"
               className={`px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 ${
-                location.pathname === 'ravaas-bagels/'
+                location.pathname === '/'
                   ? 'bg-ravaa-red text-white'
                   : 'text-white hover:bg-ravaa-red hover:text-white'
               }`}
